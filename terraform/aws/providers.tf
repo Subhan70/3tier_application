8452a1686e5ecd129b910a2faa702bf4de24a488
Rajat Sardesai <rajatsardesai@gmail.com>
2024-02-25 18:04:59 +0530

    Reverted code

--- terraform/aws/providers.tf
+++ terraform/aws/providers.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   
-  required_version = ">=1.6.3, <1.8.0"
+  required_version = "~> 1.6.3"
 }
 ##
 ##
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -27,7 +27,7 @@ terraform {
  #   region = "us-east-1" # This is the AWS default, but can be set to any valid AWS region.
  # }
 
-  required_version = ">=1.6.3, <1.8.0"
+  required_version = "~> 1.6.3"
 }
 ##
 ##
