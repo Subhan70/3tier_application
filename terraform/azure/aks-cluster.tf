515ef146748edd5864cb1eb22a0328dd95127bb3
Rajat Sardesai <rajatsardesai@gmail.com>
2024-03-01 11:20:40 +0530

    Added zones to aks node pools

--- terraform/azure/aks-cluster.tf
+++ terraform/azure/aks-cluster.tf
@@ -16,6 +16,7 @@ module "aks" {
       vnet_subnet_id        = azurerm_subnet.az-subnets.id
       max_count = 3
       min_count             = 1
+      zones                 = "1"
     }
     two = {
       name                  = "node-2"
@@ -24,6 +25,7 @@ module "aks" {
       vnet_subnet_id        = azurerm_subnet.az-subnets.id
       max_count = 2
       min_count             = 1
+      zones                 = "1"
     }
   }
 }
