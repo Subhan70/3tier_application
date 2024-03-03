0e7859c914eb397a499febc25c709885bcb93d30
Rajat Sardesai <rajatsardesai@gmail.com>
2024-03-03 21:46:32 +0530

    Changed cluster name

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -7,5 +7,5 @@ variable "region" {
 variable "clusterName" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "multicloud-threetier-eks"
+  default     = "threetier-eks"
 }
