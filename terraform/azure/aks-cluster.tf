8baced6c5e3c8b8f0feb9d257f70591bcb5eb3bb
Rajat Sardesai <rajatsardesai@gmail.com>
2024-03-04 15:20:44 +0530

    Removed role based access control

--- terraform/azure/aks-cluster.tf
+++ terraform/azure/aks-cluster.tf
@@ -7,10 +7,6 @@ module "aks" {
   os_disk_size_gb     = 60
   sku_tier            = "Standard"
   vnet_subnet_id      = azurerm_subnet.az-subnet.id
-  rbac_aad_client_app_id     = "${azuread_application.client.application_id}"
-  rbac_aad_server_app_id     = "${azuread_application.server.application_id}"
-  rbac_aad_server_app_secret = "${azuread_service_principal_password.server.value}"
-  role_based_access_control_enabled = true
   node_pools          = {
     one = {
       name                  = "node1"
