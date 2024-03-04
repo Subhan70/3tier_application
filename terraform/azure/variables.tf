d15f281ba61ebf53ac19f16728704325cc76fdc4
Rajat Sardesai <rajatsardesai@gmail.com>
2024-03-04 22:52:06 +0530

    Test

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -30,7 +30,7 @@ variable "use_for_each" {
 
 # Get the subnet ID using the data source
 data "azurerm_subnet" "subnet" {
-  name                  = "multicloud-threetier-subnets"
+  name                  = ["public-subnet-1", "public-subnet-2", "public-subnet-3", "private-subnet-1", "private-subnet-2", "private-subnet-3"]
   virtual_network_name  = module.vnet.vnet_name
   resource_group_name  = var.resourceGroup
 }
